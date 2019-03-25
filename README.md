7b2c57e2d70d4c282d48a97357f79e29309e5803
mirekuhlir <mirek.uhlir@gmail.com>
2019-03-25 10:12:51 +0100

    Readme update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Quick start
 
 - `git clone https://github.com/mirekuhlir/bsc-notes.git`
-- `cd bsc-notes`
+- `cd bsc-notes-master`
 - `npm install`
 - `npm start`
 - App is now running at `http://localhost:9000`
--- conf.js
+++ conf.js
@@ -1,6 +1,7 @@
 exports.config = {
   framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
+  directConnect: true,
   specs: ['spec.js'],
   capabilities: {
     browserName: 'chrome'
